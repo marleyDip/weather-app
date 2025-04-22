6525ee307dee6177bba7f595b24dd185d8753c3f
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-04-23 01:54:12 +0600

    handling API for search city for weather

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,68 @@
-<script setup>
-</script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a City or State"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[opx_1px_0_0_#004E71]"
+      />
+
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <p class="py-2" v-if="searchError">
+          Sorry, something went wrong, Please try again.
+        </p>
+
+        <p class="py-2" v-if="!serverError && mapboxSearchResults.length === 0">
+          No result match your query, try a different term.
+        </p>
+
+        <template v-else>
+          <li
+            v-for="SearchResult in mapboxSearchResults"
+            :key="SearchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ SearchResult.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const mapboxAPIKey =
+  "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+        ///console.log(mapboxSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
+
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
